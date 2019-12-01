19ff6ba007554c961cab455769c45b106c2e9f2e
Eduardo Rodríguez <eduzrdz30@gmail.com>
2019-11-30 18:59:46 -0600

    Add arreglo de objetos para memoria
    Se agrego la clase mem para hacer arreglos de objetos para memoria y swap. Tambien se muestran ejemplos para llamar una posicion de memoria

--- main.py
+++ main.py
@@ -6,18 +6,49 @@ import sys
 ## 2 de diciembre de 2019
 
 class process(object):
-    def __init__(self, nombre, size, inicio, swap):
+    def __init__(self, nombre, size, inicio):
         self.id = nombre
         self.tam = size
-        self.pags = math.ceil(op1/16)
+        self.pags = math.ceil(size/16)
         self.i = inicio
+        self.sw = swap
+        self.pf = 0
+
+class memoria(object):
+    def __init__(self, pos, id, val, swap):
+        self.pos = pos
+        self.id = id
+        self.val = val
         self.sw = swap
 
 arrprocess = []                         # Arreglo de objetos (Lista de procesos)
+mem = []
+swap = []
+for n in range(128):
+    mem.append(memoria(n, 0, 0, 0))
+for n in range(256):
+    swap.append(memoria(n, 0, 0, 1))
+
+#print(mem[0].pos)
+#print(mem[0].id)
+#print(mem[0].val)
+#print(mem[0].sw)
+
+swap = [] * 256
 op0 = 0                                 # En una instrucción 'A 82 1 0'
 op1 = 0                                 # op0 = A, op1 = 82, op2 = 1, op3 = 0
 op2 = 0
 op3 = 0
+swap_in = 0
+swap_out = 0
+
+def reset():
+    global arrprocess, op0, op1, op2, op3
+    arrprocess = []
+    op0 = 0
+    op1 = 0
+    op2 = 0
+    op3 = 0
 
 infile = open("input.mem", "r")         # Lee el archivo "input.mem", donde se encuentran los comandos
 texto = infile.readlines()              # Lee cada linea y las guarda en una lista
@@ -36,8 +67,18 @@ while(op0 != 'E'):
             print(n, end = ' ')
         print()
 
+    if op0 == 'P':
+        0
+
+    if op0 == 'A':
+        0
+
+    if op0 == 'L':
+        0
+        
     if op0 == 'F':                                      # Finalizan las operaciones y se reinicia todo
         print()
+        reset()
         print('== FIN DE LOS PROCESOS ==')
         print() 
         
